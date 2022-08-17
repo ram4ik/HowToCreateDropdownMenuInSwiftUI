5f8b63a35aeb9f80be6c8fabe9d57d7c59a0e3d5
Ramill <ramill_ibragimov@icloud.com>
2022-08-17 08:39:47 +0300

    Initial Commit

---
+++ HowToCreateDropdownMenuInSwiftUI/HowToCreateDropdownMenuInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToCreateDropdownMenuInSwiftUIApp.swift
+//  HowToCreateDropdownMenuInSwiftUI
+//
+//  Created by Ramill Ibragimov on 17.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct HowToCreateDropdownMenuInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
